826bcf4f762e1a8bf33955c295dc7ad6958e070a
Ian Fisher <ian@iafisher.com>
2025-09-12 23:48:48 -0400

    2025-09-trees: go avl impl seems to work

--- 2025/2025-09-trees/main.go
+++ 2025/2025-09-trees/main.go
@@ -57,6 +57,9 @@ func buildStack(n *AvlNode, key string, value string) []StackItem {
 	}
 }
 
+func rotateRight(n *AvlNode) {
+}
+
 func updateBalances(stack []StackItem) {
 	if len(stack) < 2 {
 		panic(
@@ -76,10 +79,119 @@ func updateBalances(stack []StackItem) {
 
 		if it.Node.Balance == 0 {
 			break
+		} else if it.Node.Balance == 2 {
+			rotateRight(it.Node)
 		}
 
 		isLeft = it.IsLeft
 	}
+}
+
+// precondition: n.Balance == -2
+// precondition: n.Right.Balance == -1
+func (n *AvlNode) rotateRight() *AvlNode {
+	newRoot := n.Left
+	n.Left = newRoot.Right
+	newRoot.Right = n
+	return newRoot
+}
+
+// precondition: n.Balance == 2
+// precondition: n.Right.Balance == 1
+func (n *AvlNode) rotateLeft() *AvlNode {
+	newRoot := n.Right
+	n.Right = newRoot.Left
+	newRoot.Left = n
+	return newRoot
+}
+
+func (n *AvlNode) rotateLeftRight() *AvlNode {
+	n.Left = n.Left.rotateLeft()
+	return n.rotateRight()
+}
+
+func (n *AvlNode) rotateRightLeft() *AvlNode {
+	n.Right = n.Right.rotateRight()
+	return n.rotateLeft()
+}
+
+// returns `(node, heightChange)`
+// postcondition: `node` is a balanced AVL tree
+// postcondition: `heightChange` is -1, 0, or 1
+func (n *AvlNode) insert2(key string, value string) (*AvlNode, int) {
+	if n == nil {
+		return New(key, value), 1
+	}
+
+	// logging := key == "2"
+	logging := false
+
+	myHeightChange := 0
+	var childHeightChange int
+	cmp := strings.Compare(key, n.Key)
+	if cmp < 0 {
+		n.Left, childHeightChange = n.Left.insert2(key, value)
+		if logging {
+			fmt.Printf("got %d at key=%s\n", childHeightChange, n.Key)
+		}
+		if childHeightChange != 0 && n.Balance >= 0 {
+			myHeightChange = childHeightChange
+		}
+		n.Balance += childHeightChange
+	} else if cmp == 0 {
+		n.Value = value
+		return n, 0
+	} else {
+		n.Right, childHeightChange = n.Right.insert2(key, value)
+		if childHeightChange != 0 && n.Balance <= 0 {
+			myHeightChange = childHeightChange
+		}
+		n.Balance -= childHeightChange
+	}
+
+	if n.Balance == -2 {
+		if n.Right.Balance == 1 {
+			fmt.Printf("rotateRightLeft at key=%s\n", n.Key)
+			r := n.rotateRightLeft()
+			r.Balance = 0
+			r.Left.Balance = 0
+			r.Right.Balance = 0
+			return r, 0
+		} else {
+			fmt.Printf("rotateLeft at key=%s\n", n.Key)
+			r := n.rotateLeft()
+			r.Balance = 0
+			r.Left.Balance = 0
+			return r, 0
+		}
+	} else if n.Balance == 2 {
+		if n.Left.Balance == -1 {
+			fmt.Printf("rotateLeftRight at key=%s\n", n.Key)
+			r := n.rotateLeftRight()
+			r.Balance = 0
+			r.Left.Balance = 0
+			// TODO: this seems wrong to hard-code
+			r.Right.Balance = -1
+			return r, 0
+		} else {
+			fmt.Printf("rotateRight at key=%s\n", n.Key)
+			r := n.rotateRight()
+			r.Balance = 0
+			// TODO: this seems inconsistent with the above
+			r.Right.Balance = 0
+			return r, 0
+		}
+	}
+
+	if logging {
+		fmt.Printf("returning %d from key=%s\n", myHeightChange, n.Key)
+	}
+	return n, myHeightChange
+}
+
+func (n *AvlNode) Insert2(key string, value string) *AvlNode {
+	r, _ := n.insert2(key, value)
+	return r
 }
 
 func (n *AvlNode) Insert(key string, value string) *AvlNode {
@@ -141,14 +253,34 @@ func (n AvlNode) Print() {
 }
 
 func main() {
-	root := New("50", "")
-	root = root.Insert("60", "")
-	root = root.Insert("70", "")
-	root = root.Insert("30", "")
-	root = root.Insert("20", "")
-	root = root.Insert("40", "")
-	root = root.Insert("45", "")
-	root = root.Insert("35", "")
-	root = root.Insert("10", "")
+	// root := New("50", "")
+	// root = root.Insert2("60", "")
+	// root = root.Insert2("70", "")
+	// root = root.Insert2("30", "")
+	// root = root.Insert2("20", "")
+	// root = root.Insert2("40", "")
+	// root = root.Insert2("45", "")
+	// root = root.Insert2("35", "")
+	// root = root.Insert2("10", "")
+	// root.Print()
+
+	root := New("5", "")
+	fmt.Println("insert 6")
+	root = root.Insert2("6", "")
+	root.Print()
+	fmt.Println("insert 8")
+	root = root.Insert2("8", "")
+	root.Print()
+	fmt.Println("insert 3")
+	root = root.Insert2("3", "")
+	root.Print()
+	fmt.Println("insert 2")
+	root = root.Insert2("2", "")
+	root.Print()
+	fmt.Println("insert 4")
+	root = root.Insert2("4", "")
+	root.Print()
+	fmt.Println("insert 7")
+	root = root.Insert2("7", "")
 	root.Print()
 }
